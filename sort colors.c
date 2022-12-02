f8787d966d12ed3e6b2e03e0e3e5d7972d7fb755
janhvis51 <119559319+janhvis51@users.noreply.github.com>
2022-12-02 14:10:28 +0530

    Create sort colors.c

---
+++ sort colors.c
@@ -0,0 +1,29 @@
+void sortColors(int* nums, int numsSize){
+    int i=0,j,mid=0,t;
+    j=numsSize-1;
+    while(mid<=j)
+    {
+        
+    if(nums[mid]==0)
+    {
+        t=nums[mid];
+    nums[mid]=nums[i];
+        nums[i]=t;
+        mid++;
+        i++;
+    }
+    else if(nums[mid]==1)
+    {
+        mid++;
+    }
+    else
+    {
+        t=nums[mid];
+    nums[mid]=nums[j];
+        nums[j]=t;
+        j--;
+    } 
+}
+  return nums;      
+    
+}
